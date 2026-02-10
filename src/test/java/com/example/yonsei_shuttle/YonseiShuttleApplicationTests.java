d6e57497ba5518d2523a5fd76ad8679a870f3b90
Hannah Cho <hannahcho422@gmail.com>
2026-02-11 01:21:41 +0900

    init: Initial project setup

---
+++ src/test/java/com/example/yonsei_shuttle/YonseiShuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yonsei_shuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonseiShuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
