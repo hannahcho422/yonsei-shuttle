abbc048f1c33fe47db77f9aafa8e171c9b1aea4f
Hannah Cho <hannahcho422@gmail.com>
2026-04-14 23:45:24 +0900

    refactor: reorganize project directory structure

---
+++ backend/src/main/java/com/yonsei/shuttle/YonseiShuttleApplication.java
@@ -0,0 +1,13 @@
+package com.yonsei.shuttle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YonseiShuttleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YonseiShuttleApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yonsei/shuttle/YonseiShuttleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yonsei.shuttle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YonseiShuttleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
