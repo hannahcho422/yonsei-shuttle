7a223dca17cf136510eced2c75e593796f257de2
Hannah Cho <hannahcho422@gmail.com>
2026-04-15 22:45:37 +0900

    feat: add Admin entity with one-to-one relationship to User

---
+++ backend/src/main/java/com/yonsei/shuttle/user/domain/Admin.java
@@ -0,0 +1,39 @@
+package com.yonsei.shuttle.user.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 관리자 확장 정보 (User 1:1)
+ */
+@Entity
+@Table(name = "admin", schema = "yonsei_shuttle")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Integer adminId;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
+    private User user;
+
+    @Builder
+    private Admin(User user) {
+        this.user = user;
+    }
+}
