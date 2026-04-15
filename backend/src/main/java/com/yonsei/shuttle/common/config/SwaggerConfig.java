8d4d858e734e8245e55f6067c878ed3196e417bb
Hannah Cho <hannahcho422@gmail.com>
2026-04-15 22:03:12 +0900

    feat: add Swagger configuration for API documentation

---
+++ backend/src/main/java/com/yonsei/shuttle/common/config/SwaggerConfig.java
@@ -0,0 +1,37 @@
+package com.yonsei.shuttle.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+
+/**
+ * Swagger (OpenAPI) 문서 설정
+ * 접속: http://localhost:8080/swagger-ui.html
+ */
+@Configuration
+public class SwaggerConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("연세 셔틀 API")
+                        .version("v0.0.1")
+                        .description("연세대학교 미래캠퍼스 셔틀버스 예약/조회/실시간 위치 API"))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .name(SECURITY_SCHEME_NAME)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+    }
+}
