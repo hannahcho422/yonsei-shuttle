b0444317dc825e9ba4df2bcde3b1fdd74da2951f
Hannah Cho <hannahcho422@gmail.com>
2026-04-16 01:30:42 +0900

    feat: add SignupRequest DTO for user registration

---
+++ backend/src/main/java/com/yonsei/shuttle/auth/dto/SignupRequest.java
@@ -0,0 +1,29 @@
+package com.yonsei.shuttle.auth.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+/**
+ * 회원가입 요청 DTO
+ */
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class SignupRequest {
+
+    @NotBlank(message = "이름은 필수입니다")
+    @Size(max = 100, message = "이름은 100자 이하여야 합니다")
+    private String name;
+
+    @NotBlank(message = "이메일은 필수입니다")
+    @Email(message = "이메일 형식이 올바르지 않습니다")
+    @Size(max = 100, message = "이메일은 100자 이하여야 합니다")
+    private String email;
+
+    @NotBlank(message = "비밀번호는 필수입니다")
+    @Size(min = 8, max = 50, message = "비밀번호는 8자 이상 50자 이하여야 합니다")
+    private String password;
+}
