c39828b6de351a1814c1457972b445ea888a213b
Hannah Cho <hannahcho422@gmail.com>
2026-04-17 00:13:40 +0900

    feat: add auth check on app initialization

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,16 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div id="app">
+    <router-view />
+  </div>
 </template>
 
-<style scoped></style>
+<script setup>
+import { onMounted } from 'vue'
+import { useAuthStore } from './stores/auth'
+
+const authStore = useAuthStore()
+
+onMounted(() => {
+  authStore.checkLoginStatus()
+})
+</script>
