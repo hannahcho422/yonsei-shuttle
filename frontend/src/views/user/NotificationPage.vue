bd3330983296d5144af9a8557e01ed4fc1bec088
Hannah Cho <hannahcho422@gmail.com>
2026-04-17 00:20:37 +0900

    feat: add initial page structure with placeholder components

---
+++ frontend/src/views/user/NotificationPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-5">
+    <h1>NotificationPage</h1>
+    <p>준비 중입니다.</p>
+  </div>
+</template>
---
+++ frontend/src/views/user/SignupPage.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="container mt-5">
+    <h1>SignupPage</h1>
+    <p>준비 중입니다.</p>
+  </div>
+</template>
