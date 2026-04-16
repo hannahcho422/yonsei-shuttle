0675cef4d713257b141746508204ebcc797877be
Hannah Cho <hannahcho422@gmail.com>
2026-04-17 00:15:36 +0900

    feat: add axios instance with JWT interceptors

---
+++ frontend/src/api/index.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import router from '../router'
+
+const api = axios.create({
+  baseURL: '/api',
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+})
+
+// Request interceptor: JWT 토큰 자동 첨부
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+// Response interceptor: 401 시 로그인 페이지로 리다이렉트
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      localStorage.removeItem('accessToken')
+      localStorage.removeItem('refreshToken')
+      router.push('/login')
+    }
+    return Promise.reject(error)
+  }
+)
+
+export default api
